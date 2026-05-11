720315d49f90952bc0b6e84c39772daf1c093c61
Chillbytefire <chillbytefire@gmail.com>
2026-05-11 19:21:25 +0530

    cbf's changes

--- code/cogs/commands.py
+++ code/cogs/commands.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import time
 
 from helpers.funcs import send, fmt_time, online, is_staff, ts
 from setup.config import EMBED_COLOR, LOADING, SUCCESS
@@ -11,6 +12,16 @@ def register_commands(bot):
 
     @bot.command()
     async def ping(ctx):
-        print(round(bot.latency * 1000))
+        start = time.perf_counter()
+        msg = await ctx.send("Pinging...")
+        end = time.perf_counter()
+
+        latency = round((end - start) * 1000)
+
+        gateway = round(bot.latency * 1000)
+
+        await msg.edit(
+            content=f"Pong!\nLatency: `{latency}ms`\nAPI heartbeat: `{gateway}ms`"
+        )
 
 # yes its incomplete, what did you expect from the first version LMAOOO
